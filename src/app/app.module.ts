adf3f00e132d5a486304ed5c24ba41d2e61fbc50
Rookie2907 <philip.neill@hotmail.com>
2019-12-04 14:14:02 +0000

    2 files modified, 2 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { TopbarComponent } from './topbar/topbar.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, TopbarComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
